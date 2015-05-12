ca77d8bd3d36196ea083da72e2507f9e9e6ddefe
Gianluca Bargelli <g.bargelli@gmail.com>
2015-05-12 02:15:32 +0200

    Added Cache HTTP and real API calls

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-//    baseUrl = /flights/from/DUB/to/CIA/2015-01-02/2015-10-02/250/unique/?limit=15&offset-0
 var express = require('express'),
     app     = express(),
     http    = require('http'),
-    url     = require('url'),
-    baseUrl = 'http://www.ryanair.com/en/api/2';
+    url     = require('url');
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-app.get('/api/*', function(request, response) {
+app.get('/cors/*', function(request, response) {
   var options = url.parse(request.url),
-      path = options.path.replace(/api/, '');
+      path = options.path.replace(/\/cors\//, '');
 
-  var connector = http.get(baseUrl + path, function(res) {
+  var connector = http.get(path, function(res) {
     response.setHeader('Content-Type', 'application/json');
     res.pipe(response, { end: true });
   });
